371b6c744d68d7e809e49ed0c57fe34423af6576
Dilshodjon <torex.amaki@gmail.com>
2025-09-14 23:55:24 +0500

    update requirements

--- run.sh
+++ run.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 #uvicorn src.main:app --reload
 #uvicorn src.main:app --reload
-uvicorn src.main:app --host 0.0.0.0 --port ${PORT:-8000}
+#uvicorn src.main:app --host 0.0.0.0 --port ${PORT:-8000}
+
+#!/bin/bash
+pip install -r requirements.txt
+uvicorn src.main:app --host 0.0.0.0 --port ${PORT:-8002}
